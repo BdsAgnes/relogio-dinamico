2bb5ce7f7fe0a468caa1cf1fcc32830b0e45d929
agnes barbosa <bdsagnes@gmail.com>
2022-06-14 16:33:27 -0300

    fase final

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert('olá!')
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        // BOM DIA!
+        msg.innerHTML = `Agora são ${hora} horas, Bom dia!`
+        img.src = 'img.png/manha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18){
+        // BOA TARDE!
+        msg.innerHTML = `Agora são ${hora} horas, Boa tarde!`
+        img.src = 'img.png/tarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // BOA NOITE!
+        msg.innerHTML = `Agora são ${hora} horas, Boa noite!`
+        img.src = 'img.png/noit.png'
+        document.body.style.background = '#515154'
+    }
+}
